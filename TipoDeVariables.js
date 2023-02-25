d73a90954da322de6e42187ff1a8e96f64da69e1
JARRIWON STIVEM MICAN CESPEDES <jarimices@gmail.com>
2023-02-24 21:19:41 -0500

    Typeof-Tipo de datos

---
+++ TipoDeVariables.js
@@ -0,0 +1,19 @@
+var Apellido = "Mican";
+
+console.log(Apellido)
+
+
+
+var mi_sueldo=100.000;
+
+console.log(mi_sueldo);
+
+//tipo de objeto 
+var objeto  = {
+    nombre: "Jarison",
+    Apellido:"Mican",
+    Telefono:32211245
+
+}
+
+console.log(objeto)
---
+++ typeof.js
@@ -0,0 +1,26 @@
+var edad = 145;
+// el typeof sirve para mirar que tipo de dato es 
+console.log(typeof edad);
+
+
+
+var nombre = "Jarison";
+
+console.log(typeof nombre);
+
+
+var sueldo = 15.00;
+
+console.log(typeof sueldo)
+
+
+
+var todo_de_mi = {
+    nombre:"Jarison",
+    Apellido:"Mican",
+    Programa:"ADSI",
+    Cel:3216692365,
+    Pais:"Colombia"
+
+}
+console.log(typeof todo_de_mi)
